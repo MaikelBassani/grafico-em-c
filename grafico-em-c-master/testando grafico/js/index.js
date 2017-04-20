207818f22200fa2f4bebde740a6f57873022daa2
FelipeHintz <felipe.hintz@catolicasc.org.br>
2017-04-20 01:04:37 -0300

    Add files via upload

--- grafico-em-c-master/testando grafico/js/index.js
+++ grafico-em-c-master/testando grafico/js/index.js
@@ -19,12 +19,12 @@ function geraValoresGrafico(nComparacao, nTrocas, nSegundos) {
 }
 
 function Grafico() {
-const dataset1 = [comparacao[1],comparacao[2],comparacao[2],trocas[1],trocas[2],trocas[2],segundos[1],segundos[2],segundos[2]];
-const dataset2 = [comparacao[3],comparacao[4],comparacao[5],trocas[3],trocas[4],trocas[5],segundos[3],segundos[4],segundos[5]];
-const dataset3 = [comparacao[6],comparacao[7],comparacao[8],trocas[6],trocas[7],trocas[8],segundos[6],segundos[7],segundos[8]];
-const dataset4 = [comparacao[9],comparacao[10],comparacao[11],trocas[9],trocas[10],trocas[11],segundos[9],segundos[10],segundos[11]];
-const dataset5 = [comparacao[12],comparacao[13],comparacao[14],trocas[12],trocas[13],trocas[14],segundos[12],segundos[13],segundos[14]];
-const width = 12000
+const dataset1 = [comparacao[0],trocas[0],segundos[0],comparacao[1],trocas[1],segundos[1],comparacao[2],trocas[2],segundos[2]];
+const dataset2 = [comparacao[3],trocas[3],segundos[3],comparacao[4],trocas[4],segundos[4],comparacao[5],trocas[5],segundos[5]];
+const dataset3 = [comparacao[6],trocas[6],segundos[6],comparacao[7],trocas[7],segundos[7],comparacao[8],trocas[8],segundos[8]];
+const dataset4 = [comparacao[9],trocas[9],segundos[9],comparacao[10],trocas[10],segundos[10],comparacao[11],trocas[11],segundos[11]];
+const dataset5 = [comparacao[12],trocas[12],segundos[12],comparacao[13],trocas[13],segundos[13],comparacao[14],trocas[14],segundos[14]];
+const width = 4000
 const height = 300
 const rectHeight = 25
 
